db3fc4715a8fda048b945e34522b72f885d6602e
Mick <mfrstatements@yahoo.com>
2020-03-24 17:03:20 -0400

    type: code
    description: Added solution for 1.9
    
    bugs fixed: none.

---
+++ 1_9_CtCI_StringRotation.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// pg 91, 1.8
+
+// Check if string is one away
+bool StringRotation(string input_string)
+{
+  return true;
+}
+
+int main(){
+	return 0;
+}
+
+
+/*
+Output:
+$g++ -o main *.cpp
+$main
+
+
+Time Complexity:
+
+Space Complexity:
+*/
